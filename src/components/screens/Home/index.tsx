7d41791fb80aa00ab87d5f46ce50fa2abb855ec8
Atabek <atabek122002@gmail.com>
2024-12-04 17:31:31 +0500

    Update Home Page

--- src/components/screens/Home/index.tsx
+++ src/components/screens/Home/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home: React.FC = () => {
+  const navigate = useNavigate();
+  React.useEffect(() => navigate('/users'), [navigate]);
   return <h1>Главная</h1>;
 };
 
